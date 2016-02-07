134c3d7885992f5d4fe4ab399bb449a3bb258913
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-02-06 19:43:57 -0600

    first commit

---
+++ src/main/java/org/javabrains/lambda/App.java
@@ -0,0 +1,29 @@
+package org.javabrains.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App
+{
+    public static void main( String[] args ) {
+
+        List<Person> persons = Arrays.asList(new Person("Mayank", 23), new Person("John", 19), new Person("Chris", 15), new Person("Brian", 16));
+
+        persons.forEach(p -> System.out.println(p));
+        printLine();
+
+        persons.forEach(p -> p.setLastName("Gupta"));
+        persons.forEach(p -> System.out.println(p));
+        printLine();
+
+        List<Person> personsOver18 = persons.stream().filter(p -> p.age > 18).collect(Collectors.toList());
+        personsOver18.forEach(p -> System.out.println(p));
+        printLine();
+
+    }
+
+    public static void printLine() {
+        System.out.println("\n=============================================\n");
+    }
+}
---
+++ src/main/java/org/javabrains/lambda/Person.java
@@ -0,0 +1,58 @@
+package org.javabrains.lambda;
+
+/**
+ * Created by Mayank on 2/6/2016.
+ */
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+
+
+    public Person(String firstName){
+        this.firstName = firstName;
+    }
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, int age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
