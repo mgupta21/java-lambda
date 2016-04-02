05eba851686bb06a4b4e3576980eb8034c9a3e48
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-04-02 10:43:46 -0500

    Added first demo

--- src/main/java/org/mgupta/streams/Demo1.java
+++ src/main/java/org/mgupta/streams/Demo1.java
@@ -1,11 +1,31 @@
 package org.mgupta.streams;
 
+import java.util.Random;
+
 /**
  * Created by mgupta on 4/2/16.
  */
 public class Demo1 {
 
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+
+        Demo1 demo = new Demo1();
+
+        String[] names = {"Mayank", "Karan", "", "Sahil", ""};
+
+        int count = 10;
+        while (count-- > 0) {
+            Random random = new Random();
+            int x = random.nextInt(5);
+            demo.testGreeting(names[x], a -> !a.isEmpty() ? "Howdy " + a + "!" : "Did you forget something?");
+        }
+    }
+
+    public void testGreeting(String str, Greeting g) {
+        System.out.println("Greeting : " + g.sayHello(str));
+    }
+
+    interface Greeting {
+        String sayHello(String str);
+    }
 }
