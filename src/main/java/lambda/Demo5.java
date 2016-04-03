e7800ae5e952c3f8b2f74d41058a2da6abf474f7
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-04-03 13:00:50 -0500

    Added notes in comments

--- src/main/java/lambda/Demo5.java
+++ src/main/java/lambda/Demo5.java
@@ -16,8 +16,8 @@ public class Demo5 {
         trades.add(new Trade("Apple", 2450, Status.PENDING));
 
         trades.forEach(t -> {
-            System.out.println("****\nTrade Name : " + t.getName());
-            System.out.println("Quantity : " + t.getQuantity());
+            System.out.println("****\nTrade Name : " + t.getName()); // We can create another interface that has a method that returns String
+            System.out.println("Quantity : " + t.getQuantity()); // We can create another interface that has a method that returns int
             System.out.println("Big Trade ? : " + isBigTrade(t));
             System.out.println("Open Trade ? : " + isOpenTrade(t));
             System.out.println("Trade Pending ? : " + checkStatus(t, Status.PENDING));
@@ -26,18 +26,27 @@ public class Demo5 {
 
     }
 
-    // Lambda behaviour is capture in a method
-    public static boolean isBigTrade(Trade trade) {
-        ITrade tradeLambda = t -> t.isBigTrade();
-        return tradeLambda.check(trade);
-    }
-
     public static boolean isOpenTrade(Trade trade) {
         ITrade tradeLambda = t -> t.isOpen();
         return tradeLambda.check(trade);
     }
 
+    public static boolean isBigTrade(Trade trade) {
+        ITrade tradeLambda = t -> t.isBigTrade();
+        return tradeLambda.check(trade);
+    }
+
+    // Lambda behaviour is capture in a method
     public static boolean checkStatus(Trade trade, Status status) {
+        // status = Status.OPEN; // Compilation error : variable used in lambda expression should be final or effectively final
+        // Note : local variables are treated as immutable in java as they exist on stack memory  as opposed to heap
+        // for instance variables hence are treated as thread safe as no other thread can access them other than they itself
+        // Although compiler assist in preventing form re-assigning local variables used in lambda expression but in case of
+        // instance variable its developer's responsibility to prevent reassigning or mutate instance variables used in lambda expression
+        // The reason is the thread that created the lambda expression (that uses modified instance variable) may have been recycled
+        // but lambda expression may still be running. Hence the code will not be thread safe.
+
+        // Lambda expression that uses parameter (status) from enclosing scope is called capturing lambda
         ITrade tradeLambda = t -> t.checkStatus(status);
         return tradeLambda.check(trade);
     }
