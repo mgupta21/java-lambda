2bb02204e5ae09679584d10c159a348efe092270
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-04-10 12:42:26 -0500

    Added demo 0

---
+++ src/main/java/lambda/Demo0.java
@@ -0,0 +1,23 @@
+package lambda;
+
+/**
+ * Created by Mayank on 4/10/2016.
+ */
+public class Demo0 {
+
+    // Optional annotation. Enforces single method to interface so it can be used for lambda expression
+    @FunctionalInterface
+    interface Multiplier {
+        int multiply(int a, int b);
+    }
+
+    Multiplier MultiplierLambda = (a, b) -> a * b;
+
+    // Type is optional and is inferred from the interface
+    //Multiplier MultiplierLambda2 = (int a , int b) -> a*b;
+
+    public static void main(String[] args) {
+        System.out.println(new Demo0().MultiplierLambda.multiply(3, 4));
+    }
+
+}
