7fa45ccbde6302355a78a1ce307d458136cc524c
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-04-10 19:42:46 -0500

    Added demo 3

---
+++ src/main/java/org/java8/interfaces/demo3/Demo3.java
@@ -0,0 +1,31 @@
+package org.java8.interfaces.demo3;
+
+/**
+ * Created by Mayank on 4/10/2016.
+ */
+// Multiple Inheritance
+
+interface Engine {
+    default String model() {
+        return "DEFAULT ENGINE";
+    }
+}
+
+interface Vehicle {
+    default String model() { // Duplicate method as in Engine
+        return "DEFAULT VEHICLE";
+    }
+}
+
+class Car implements Vehicle, Engine {
+    public String model() { // If model is not defined here compiler contains
+        // return "DEFAULT CAR";
+        return Engine.super.model();
+    }
+}
+
+public class Demo3 {
+    public static void main(String[] args) {
+        System.out.println(new Car().model());
+    }
+}
