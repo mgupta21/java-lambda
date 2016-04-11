0eacc89bc17cf86a5855dc935091c6a4cbfa41a8
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-04-10 19:34:50 -0500

    Added demo2

--- src/main/java/org/java8/interfaces/demo2/Demo2.java
+++ src/main/java/org/java8/interfaces/demo2/Demo2.java
@@ -3,7 +3,29 @@ package org.java8.interfaces.demo2;
 /**
  * Created by Mayank on 4/10/2016.
  */
+
+interface Engine {
+    default String make() {
+        return "DEFAULT MAKE";
+    }
+}
+
+interface Vehicle {
+    default String model() {
+        return "DEFAULT MODEL";
+    }
+}
+
+class Car implements Engine, Vehicle {
+    String makeAndModel() {
+        return Engine.super.make() + " " + Vehicle.super.model();
+    }
+}
+
 public class Demo2 {
 
+    public static void main(String[] args) {
+        System.out.println(new Car().makeAndModel());
+    }
 
 }
