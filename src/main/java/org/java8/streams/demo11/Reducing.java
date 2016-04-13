4df3d63c24357b1a35a28dbc2adc4da0b30b9fe4
Mayank Gupta <mayank.gupta1502@gmail.com>
2016-04-12 23:54:13 -0500

    Added demo on streams reducing

---
+++ src/main/java/org/java8/streams/demo11/Reducing.java
@@ -0,0 +1,50 @@
+package org.java8.streams.demo11;
+
+import org.java8.datamodel.trade.Trade;
+import org.java8.util.TradeUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by Mayank on 4/12/2016.
+ */
+public class Reducing {
+
+    private static List<Trade> trades = TradeUtil.createTrades();
+
+    private static void tradeNamesList() {
+        Optional<String> reducedTradeNamesList = trades.stream()
+                .map(Trade::getName)
+                .reduce((a, b) -> a + ", " + b);
+
+        System.out.println("\nComma Separated Trade Names :\n" + reducedTradeNamesList.get());
+    }
+
+    private static void tradeQuantitySum() {
+        Optional<Integer> reducedTradeQuantity = trades.stream()
+                .map(Trade::getQuantity)
+                .reduce((a, b) -> a + b);
+
+        System.out.println("\nTotal Trade Quantity :\n" + reducedTradeQuantity.get());
+    }
+
+    private static void arraysSum() {
+        List<Integer> ints = Arrays.asList(11, 12, 13, 14);
+
+        Optional<Integer> reducedOptionalSum = ints.stream().reduce((i, j) -> i + j);
+        // When we provide initial value, that possibility of null pointer disappears
+        int reducedSum = ints.stream().reduce(10, (i, j) -> i + j);
+
+        System.out.println("\nSum of arrays 1 : " + reducedOptionalSum.get());
+        System.out.println("\nSum of arrays 2 : " + reducedSum);
+    }
+
+    public static void main(String[] args) {
+        tradeNamesList();
+        tradeQuantitySum();
+        arraysSum();
+    }
+
+}
